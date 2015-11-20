8a321d07dd58aa139314a973a7993bfc3e1ebea5
astar <astarfryazino@gmail.com>
2015-11-21 00:10:56 +0300

    Refactored

--- task1.py
+++ task1.py
@@ -1,11 +1,11 @@
 def main():
 	print 'task 1'
 	n = 1000
-	sum = 0
+	s = 0
 	for i in range(n):
 		if i % 3 == 0 or i % 5 == 0:
-			sum = sum + i
-	print sum
+			s += i
+	print s
 
 if __name__ == '__main__':
 	main()
