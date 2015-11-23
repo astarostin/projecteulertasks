5431d07bcdc0ad009212f910928b93f977f41866
astar <astarfryazino@gmail.com>
2015-11-23 23:34:17 +0300

    Refactored

--- task34.py
+++ task34.py
@@ -1,21 +1,24 @@
-data = { 1:1, 2:2, 3:6, 4:24, 5:120,
-         6:720, 7:5040, 8:40320, 9:362880, 0:1 }
+data = {1: 1, 2: 2, 3: 6, 4: 24, 5: 120,
+		6: 720, 7: 5040, 8: 40320, 9: 362880, 0: 1}
+
 
 def main():
-    print 'task 34'    
-    res = 0
-    for i in range(3, 1000000):
-        res += test(i)
-    print res
+	print 'task 34'
+	res = 0
+	for i in range(3, 1000000):
+		res += test(i)
+	print res
+
 
 def test(n):
-    s = 0
-    for c in str(n):
-        s += data[int(c)]
-    if s == n:
-        return n
-    else:
-        return 0
+	s = 0
+	for c in str(n):
+		s += data[int(c)]
+	if s == n:
+		return n
+	else:
+		return 0
+
 
 if __name__ == '__main__':
-    main()
+	main()
--- task36.py
+++ task36.py
@@ -1,19 +1,21 @@
-
 def main():
-    print 'task 36'
-    res = 0
-    for i in range(1000001):
-        if is_plndr(str(i)) and is_plndr(to_bin(i)):
-            #print i
-            res += i
+	print 'task 36'
+	res = 0
+	for i in range(1000001):
+		if is_plndr(str(i)) and is_plndr(to_bin(i)):
+			# print i
+			res += i
 
-    print res    
+	print res
+
 
 def to_bin(n):
-    return bin(n)[2:]
-    
+	return bin(n)[2:]
+
+
 def is_plndr(s):
-    return s == s[::-1]
+	return s == s[::-1]
+
 
 if __name__ == '__main__':
-    main()
+	main()
