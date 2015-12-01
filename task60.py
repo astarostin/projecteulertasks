c4989b1d7125b60cdd15bf8982ff4040916b5390
Andrey Starostin <starostin@GRIDNINE.LOC>
2015-12-01 19:51:02 +0300

    task60

--- task60.py
+++ task60.py
@@ -3,13 +3,15 @@ import utils
 
 def main():
 	print 'task 60'
-	limit = 1000000
+	limit = 10000000
+	min_sum_found_so_far = 76721
 	primes = [str(n) for n in utils.primes_eratosthenes_bound(limit)]
-	magic_five_found_so_far = [[3, 31, 4159, 17209, 309571], [3, 31, 3637, 248473, 389401],  [3, 37, 67, 5923, 194119], [3, 37, 607, 276517, 495181], [3, 37, 1699, 208657, 281557], [19, 31, 4723, 221227, 376633]]		
-	min_sum_found_so_far = 200149
+	primes2 = utils.primes_eratosthenes_bound(min_sum_found_so_far + 1)
+	magic_five_found_so_far = [[3, 31, 4159, 17209, 309571], [3, 31, 3637, 248473, 389401],  [3, 37, 67, 5923, 194119], [3, 37, 607, 276517, 495181], [3, 37, 1699, 208657, 281557], [19, 31, 4723, 221227, 376633], [7, 19, 8941, 51133, 171533], [733, 883, 991, 18493, 55621]]
+	# (733, 883, 991, 18493, 55621)	
 	sum_min = 1000000
 	best_five = None
-	fives = get_magic_fives(primes, min_sum_found_so_far)
+	fives = get_magic_fives(primes, primes2, min_sum_found_so_far)
 	print fives
 	
 	for five in fives:
@@ -23,7 +25,7 @@ def main():
 	print sum_min			
 
 	
-def get_magic_fives(primes, min_sum_found_so_far):
+def get_magic_fives(primes, primes2, min_sum_found_so_far):
 	threes = set()
 	fours = set()
 	fives = set()
@@ -38,7 +40,9 @@ def get_magic_fives(primes, min_sum_found_so_far):
 			if int(s1) > min_sum_found_so_far or int(s2) > min_sum_found_so_far:
 				continue
 			if s1 in primes and s2 in primes and s2 + s1 in primes:
-				pairs.add((min(int(s1), int(s2)), max(int(s1), int(s2))))				
+				pairs.add((min(int(s1), int(s2)), max(int(s1), int(s2))))
+
+	print 'Got pairs'
 
 	for pair in pairs:
 		if pair[0] not in pairs_map.keys():
@@ -49,6 +53,8 @@ def get_magic_fives(primes, min_sum_found_so_far):
 			pairs_map[pair[1]] = set([pair[0]])
 		else:
 			pairs_map[pair[1]].add(pair[0])		
+			
+	print 'Got pairs_map'
 
 
 	for i in pairs_map.keys():
@@ -56,12 +62,11 @@ def get_magic_fives(primes, min_sum_found_so_far):
 			if i < j and i in pairs_map[j] and j in pairs_map[i]:
 				ij = pairs_map[i] & pairs_map[j]
 				for k in ij:
-					threes.add((i, j, k))				
+					threes.add((i, j, k))	
+
+	print 'Got threes'
 	
-	for p in primes:		
-		p = int(p)
-		if p > min_sum_found_so_far:
-			break
+	for p in primes2:				
 		for three in threes:
 			if p not in three:
 				combines = True
@@ -73,11 +78,10 @@ def get_magic_fives(primes, min_sum_found_so_far):
 					t = list(three)
 					t.append(p)
 					fours.add(tuple(sorted(t)))	
+	
+	print 'Got fours'
 				
-	for p in primes:
-		p = int(p)
-		if p > min_sum_found_so_far:
-			break
+	for p in primes2:				
 		for four in fours:
 			if p not in four:
 				combines = True
@@ -89,6 +93,7 @@ def get_magic_fives(primes, min_sum_found_so_far):
 					t = list(four)
 					t.append(p)
 					fives.add(tuple(sorted(t)))
+	print 'Got fives'
 	
 	return fives
 
