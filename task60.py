a1415e03aaea00e08392b4c73e43d92debcaf052
astar <astarfryazino@gmail.com>
2015-12-03 01:18:48 +0300

    task 60

--- task60.py
+++ task60.py
@@ -11,23 +11,14 @@ cache_true_pair = set()
 def main():
 	print 'task 60'
 
-	min_sum_found_so_far = 76721
+	min_sum_found_so_far = 34427
 	#primes = [str(n) for n in utils.primes_eratosthenes_bound(limit)]
-	primes2 = utils.primes_eratosthenes_bound(min_sum_found_so_far + 1)
-
-	magic_five_found_so_far = [[3, 31, 4159, 17209, 309571], [3, 31, 3637, 248473, 389401],
-							   [3, 37, 67, 5923, 194119], [3, 37, 607, 276517, 495181], [3, 37, 1699, 208657, 281557],
-							   [19, 31, 4723, 221227, 376633], [7, 19, 8941, 51133, 171533],
-							   [733, 883, 991, 18493, 55621]]
-	# (733, 883, 991, 18493, 55621)	
-	f = open('data/task60_fives_known.txt', 'w')
-	for five in magic_five_found_so_far:
-		f.write(' '.join([str(c) for c in five]) + '\n')
-	f.close()
+	primes2 = utils.primes_eratosthenes_bound(min_sum_found_so_far)
+	the_answer_so_far = [7, 1237, 2341, 12409, 18433] # 34427
 	sum_min = 1000000
 	best_five = None
 	fives = get_magic_fives(primes2, min_sum_found_so_far)	
-	
+	print fives
 	for five in fives:
 		s = sum(five)
 		if s < sum_min:
@@ -42,92 +33,86 @@ def get_magic_fives(primes2, min_sum_found_so_far):
 	threes = set()
 	fours = set()
 	fives = set()
-	# pairs = set()
 	pairs_map = {}
-	# for s in primes:
-	# 	for i in xrange(1, len(s)):
-	# 		if s[i] == '0':
-	# 			continue
-	# 		s1 = s[:i]
-	# 		s2 = s[i:]
-	# 		if int(s1) > min_sum_found_so_far or int(s2) > min_sum_found_so_far:
-	# 			continue
-	# 		if s1 in primes and s2 in primes and s2 + s1 in primes:
-	# 			pairs.add((min(int(s1), int(s2)), max(int(s1), int(s2))))
-
-	it = 0
-	for i in xrange(len(primes2) - 1):
-		n1 = primes2[i]
-		if it > 4000000:
-			break
-		for j in xrange(i + 1, len(primes2)):
-			it += 1
-			if it % 100000 == 0:				
-				print it, len(pairs_map.keys()), len(cache_true)# , len(cache_false)			
-				cache_true.clear()			
-				
-			n2 = primes2[j]
-			#if (str(n1) + str(n2)) in primes3 and (str(n2) + str(n1)) in primes3:
-			if test_primes(n1, n2):
-				if n1 not in pairs_map:
-					pairs_map[n1] = {n2}
-				else:
-					pairs_map[n1].add(n2)
-				if n2 not in pairs_map:
-					pairs_map[n2] = {n1}
-				else:
-					pairs_map[n2].add(n1)
-	cache_true.clear()
-
-	# print 'Got pairs'
+	# it = 0
+	# for i in xrange(len(primes2) - 1):
+	# 	n1 = primes2[i]
+	# 	if it > 4000000:
+	# 		break
+	# 	for j in xrange(i + 1, len(primes2)):
+	# 		it += 1
+	# 		if it % 100000 == 0:
+	# 			print it, len(pairs_map.keys()), len(cache_true)# , len(cache_false)
+	# 			cache_true.clear()
 	#
-	# for pair in pairs:
-	# 	if pair[0] not in pairs_map.keys():
-	# 		pairs_map[pair[0]] = {pair[1]}
-	# 	else:
-	# 		pairs_map[pair[0]].add(pair[1])
-	# 	if pair[1] not in pairs_map.keys():
-	# 		pairs_map[pair[1]] = {pair[0]}
-	# 	else:
-	# 		pairs_map[pair[1]].add(pair[0])
-			
-	print 'Got pairs_map', len(pairs_map.keys())
-
-	for i in pairs_map:
-		for j in pairs_map:
-			if i < j and i in pairs_map[j] and j in pairs_map[i]:
-				ij = pairs_map[i] & pairs_map[j]
-				for k in ij:
-					if i + j + k < min_sum_found_so_far:
-						threes.add((i, j, k))				
-
-	print 'Got threes'
-	f = open('data/task60_threes.txt', 'w')
-	for three in threes:
-		f.write(' '.join([str(c) for c in three]) + '\n')
+	# 		n2 = primes2[j]
+	# 		#if (str(n1) + str(n2)) in primes3 and (str(n2) + str(n1)) in primes3:
+	# 		if test_primes(n1, n2):
+	# 			if n1 not in pairs_map:
+	# 				pairs_map[n1] = {n2}
+	# 			else:
+	# 				pairs_map[n1].add(n2)
+	# 			if n2 not in pairs_map:
+	# 				pairs_map[n2] = {n1}
+	# 			else:
+	# 				pairs_map[n2].add(n1)
+	# cache_true.clear()
+	# del primes2[:]
+	#
+	# print 'Got pairs_map', len(pairs_map.keys())
+	# primes_in_threes = set()
+	# for i in pairs_map:
+	# 	for j in pairs_map:
+	# 		if i < j and i in pairs_map[j] and j in pairs_map[i]:
+	# 			ij = pairs_map[i] & pairs_map[j]
+	# 			for k in ij:
+	# 				if i + j + k < min_sum_found_so_far:
+	# 					threes.add((i, j, k))
+	# 					primes_in_threes.add(i)
+	# 					primes_in_threes.add(j)
+	# 					primes_in_threes.add(k)
+	#
+	# print 'Got threes'
+	# f = open('data/task60_threes.txt', 'w')
+	# for three in threes:
+	# 	f.write(' '.join([str(c) for c in three]) + '\n')
+	# f.close()
+	#
+	# print len(primes_in_threes) * len(threes)
+	# it = 0
+	# for p in primes_in_threes:
+	# 	for three in threes:
+	# 		it += 1
+	# 		if it % 100000 == 0:
+	# 			print it, len(fours)
+	# 		if p not in three and sum(three) + p < min_sum_found_so_far:
+	# 			combines = True
+	# 			for n in three:
+	# 				if test_primes(p, n) is not True:
+	# 					combines = False
+	# 					break
+	# 			if combines is True:
+	# 				t = list(three)
+	# 				t.append(p)
+	# 				fours.add(tuple(sorted(t)))
+	#
+	# print 'Got fours'
+	# f = open('data/task60_fours.txt', 'w')
+	# for four in fours:
+	# 	f.write(' '.join([str(c) for c in four]) + '\n')
+	# f.close()
+	f = open('data/task60_fours.txt', 'r')
+	for four in f:
+		fours.add(tuple([int(n) for n in four.split()]))
 	f.close()
-	
-	for p in primes2:				
-		for three in threes:
-			if p not in three and sum(three) + p < min_sum_found_so_far: 
-				combines = True
-				for n in three:
-					if test_primes(p, n) is not True:
-						combines = False
-						break
-				if combines is True:
-					t = list(three)
-					t.append(p)
-					fours.add(tuple(sorted(t)))	
-	
-	print 'Got fours'
-	f = open('data/task60_fours.txt', 'w')
-	for four in fours:
-		f.write(' '.join([str(c) for c in four]) + '\n')
-	f.close()
-				
-	for p in primes2:				
+
+	print len(primes2) * len(fours)
+	it = 0
+	for p in primes2:
 		for four in fours:
+			it += 1
+			if it % 100000 == 0:
+				print it, len(fives)
 			if p not in four and sum(four) + p < min_sum_found_so_far:
 				combines = True
 				for n in four:
