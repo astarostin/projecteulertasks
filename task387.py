e7ffd511ed199c2150de31d87c32a419cd4317cf
Andrey Starostin <starostin@GRIDNINE.LOC>
2015-12-11 12:53:09 +0300

    task387

--- task387.py
+++ task387.py
@@ -2,68 +2,40 @@ import math
 import utils
 
 def main():
-	print 'task387'	
+	print 'task387'		
 	
-	n = 0
-	limit = 1000000
-	res = 0
-	start = 11
-	s = 9
-	
-	for n in xrange(start, limit, 2):
-		if n % 10000 == 1:
-			s = sum([int(c) for c in str(n)])
-		elif n % 1000 == 1:
-			s -= 25
-		elif n % 100 == 1:
-			s -= 16
-		elif n % 10 == 1:
-			s -= 7			
-		else:
-			s += 2
-		if is_prime_strong_and_rth(n, s):
-			res += n		
-			
+	res = 0	
+	rth_strong_list = generate_strong_harshad(13)	
+	for n in rth_strong_list:	
+		n *= 10
+		for c in [1, 3, 5, 7, 9]:
+			num = n + c
+			if utils.is_prime_simple(num):
+				res += num
 	print res			
 	
-	
-def is_harshad(n, s):
-	if n == 0:
-		return True
-	return n % s == 0
-	
-	
-def is_rth(n, s):
-	if n / 10 == 0:
-		return True	
-	s -= n % 10
-	return is_rth_rec(n / 10, s)
-	
 
-def is_rth_rec(n, s):
-	if n / 10 == 0:
-		return is_harshad(n, s)
-	if not is_harshad(n, s):
-		return False
-	s -= n % 10
-	return is_rth_rec(n / 10, s)
-	
-	
-def is_strong_harshad(n, s):
-	if n == 0:
-		return True	
-	return n % s == 0 and utils.is_prime_simple(n / s)
-	
-def is_prime_strong_and_rth(n, s):
-	if not utils.is_prime_simple(n):
-		return False
-	s -= n % 10
-	if not is_harshad(n / 10, s):
-		return False
-	return is_strong_harshad(n / 10, s) and is_rth(n / 10, s)
+def generate_strong_harshad(limit):
+	initial = [x for x in xrange(1, 10)]
+	res = []	
+	it = 0
+	while it < len(initial):
+		n = initial[it]
+		if len(str(n)) >= limit:
+			it +=1
+			continue
+		s = sum([int(c) for c in str(n)])
+		n *= 10
+		for c in xrange(0, 10):
+			num = n + c
+			if num % (s + c) == 0:
+				if utils.is_prime_simple(num / (s + c)):					
+					res.append(num)
+				initial.append(num)
+				
+		it += 1
+	return res	
 
-def is_prime(n):
-	return n in primes[n / 1000]
 	
 if __name__ == '__main__':
 	main()
